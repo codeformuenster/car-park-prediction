8c429399b1617d5d2eb57d35c47733721c1daf9f
ThorbenJensen <jensen.thorben@gmail.com>
2017-05-23 21:56:16 +0200

    starting to use elasticsearch package

--- src/get_data.py
+++ src/get_data.py
@@ -5,17 +5,16 @@ Created on Tue May 23 21:34:02 2017
 @author: thor
 """
 
-import urllib.request
-import pandas as pd
+#%% PACKAGE
 
-#%%
-
+from elasticsearch import Elasticsearch
 api_link = 'https://elasticsearch.kube.codeformuenster.org/parkleit2/_search'
-result = urllib.request.urlopen(api_link).read()
-pd.read_json(result)
+es = Elasticsearch(api_link, verify_certs=True)
+result = es.search()
 
+result
+# TODO: get useful data
 
-#%% TODO: get time series
 
 
 

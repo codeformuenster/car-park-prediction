9a7ed09e5c7258bd7c70e29730b8143d5901e5e9
ThorbenJensen <jensen.thorben@gmail.com>
2017-05-25 12:41:26 +0200

    read all data from elasticsearch database and stored to SQLite; NEXT: explore time series and visualize.

---
+++ src/elasticsearch_to_sqlite.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 11:28:15 2017
+
+@author: thor
+"""
+
+from elasticsearch import Elasticsearch
+import pandas as pd
+import sqlite3
+
+#%% setup elasticsearch
+
+api_link = 'https://elasticsearch.kube.codeformuenster.org'
+es = Elasticsearch(api_link, 
+                   verify_certs=True)
+
+#%% page to pandas
+
+def page_to_df(page):
+    hits = page['hits']['hits']
+    observations = [ x['_source'] for x in hits]
+    data_all = pd.DataFrame(observations)
+    data_select = data_all[['name', 'timestamp', 'free']]
+    df = data_select.dropna(axis=0, how='any')
+    return df
+
+#%% scroll
+
+page = es.search(
+    index='parkleit2',
+    scroll = '10m',
+    size = 10000,
+    body={"query": {"match_all": {}}}
+)
+
+sid = page['_scroll_id']
+scroll_size = page['hits']['total']
+
+# store hits locally
+df_all = page_to_df(page)
+
+# report on progress
+total_size = scroll_size
+print('Total scroll size: %s' % total_size)
+retrieved = 0
+
+while (scroll_size > 0):
+    print ("Scrolling...")
+    
+    page = es.scroll(scroll_id = sid, scroll = '10m')
+    sid = page['_scroll_id']  # Update the scroll ID
+
+    # store hits locally
+    if len(page['hits']['hits']) > 0:
+        df_new = page_to_df(page)
+        df_all = pd.concat([df_all, df_new])
+        print('Items stored in "df_all": %i' % len(df_all.index))
+
+    # report on progress
+    scroll_size = len(page['hits']['hits'])
+    print ("scroll size: %i" % scroll_size)
+    retrieved += scroll_size
+    print ('Retrieved %i of %i' % (retrieved, total_size))
+
+#%% store in local db
+
+con = sqlite3.connect("../database/parkleit2.sqlite")
+df_all.to_sql(name="parkleit2", con=con, if_exists="replace")
+con.close()
--- src/get_data.py
+++ src/get_data.py
@@ -5,57 +5,15 @@ Created on Tue May 23 21:34:02 2017
 @author: thor
 """
 
-import json
+import sqlite3
 import pandas as pd
 
-from urllib.request import urlopen
+#%% get DataFrame from SQLite
 
+con = sqlite3.connect("../database/parkleit2.sqlite")
+df = pd.read_sql(sql="SELECT * FROM parkleit2", con=con)
+con.close()
 
-#%% package elasticsearch
-# TODO: test scrolling
+#%% TODO: visualize time series
 
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
-
-api_link = 'https://elasticsearch.kube.codeformuenster.org/parkleit2/_search'
-es = Elasticsearch(api_link, 
-                   verify_certs=True)
-
-s = Search(using=es)
-s = s.query("match_all", fields=[])
-s.execute()
-
-#s.fields([])
-#ids = [h.meta.id for h in s.scan()]
-
-#es.search()
-#
-#result
-
-
-#%% get json
-
-size = 10000
-api_link = \
-    'https://elasticsearch.kube.codeformuenster.org/parkleit2/_search?size=' + str(size)
-response = urlopen(api_link)
-result = response.read().decode('utf-8')
-
-#%% Extract hits from json
-
-parsed = json.loads(result)
-#print(json.dumps(parsed, indent=2, sort_keys=False))
-hits = parsed['hits']['hits']
-observations = [ x['_source'] for x in hits ]
-print('Retrieved %s hits form API.' % len(hits))
-
-#%% hits to Dataframe, extract (name, timestamp, free)
-
-data_all = pd.DataFrame(observations)
-data_select = data_all[['name', 'timestamp', 'free']]
-df = data_select.dropna(axis=0, how='any')
-
-#%% inspect individual timeline
-
-df2 = df.loc[df['name'] == 'PH Aegidii']
-df2 = df2.sort(columns='timestamp')
+df

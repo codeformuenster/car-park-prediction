5c1a6c1fb510f95e8dcec486a4ee66eaac02de31
ThorbenJensen <jensen.thorben@gmail.com>
2017-06-06 22:09:49 +0200

    added the previously announced R script...

---
+++ src/model_evaluation.R
@@ -0,0 +1,17 @@
+### EVALUATING PREDICTION QUALITY OF FORECASTING MODELS
+
+### LOAD LIBRARIES ############################################################
+
+library(RSQLite)
+
+### LOAD DATA #####
+
+# TODO
+
+### VISUALIZE #####
+
+# TODO
+
+### VISUALIZE PREDICTION #####
+
+
